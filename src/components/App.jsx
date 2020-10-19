36eecc3a7d3000f7ff28f71f6f74f10309a57dc4
wwperry94 <wwperry94@gmail.com>
2020-10-19 14:25:10 -0500

    first reaction

---
+++ src/components/App.jsx
@@ -0,0 +1,57 @@
+import React, { Component, Fragment } from 'react';
+
+// let App = () => {
+// return (<h1>Hello World</h1>)
+// };
+
+// export default App
+
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            text: "Confirm Age to Enter Site",
+            placeholder: "Age",
+            hasLoaded: false,
+        }
+    }
+    componentDidMount() {
+        this.setState({hasLoaded: true});
+
+    }
+    handleChange = (e) => {
+        let { value } = e.target;
+        this.setState({ inputText: value });
+
+    };
+    handleClick = async () => {
+        await this.setState({ hasLoaded: !this.state.hasLoaded });
+        if (this.state.hasLoaded) {
+            this.setState({ text: "Welcome!" });
+        } else {
+            this.setState({ text: "Loading..." });
+        };
+    };
+    cardContent = () => {
+        let { hasLoaded, inputText, placeholder } = this.state;
+
+        if (hasLoaded) {
+            return (
+                <Fragment>
+                    <h1>{this.props.h1}</h1>
+                    <input value={inputText} type="search" name="value" id="search" placeholder={placeholder} onChange={this.handleChange}></input>
+                </Fragment>
+            );
+        };
+    }
+    render() {
+        let { text } = this.state;
+        return (
+            <Fragment>
+                <h1 style={{ color: "red" }}>{text}</h1>
+                {this.cardContent()}
+                <button onClick={this.handleClick}>Step 8</button>
+            </Fragment>
+        )
+    };
+};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {render} from 'react-dom'
+import App from './components/App';
+
+render( < App
+    h1 = {'step 6'}
+    />  , document.getElementById('root'))
